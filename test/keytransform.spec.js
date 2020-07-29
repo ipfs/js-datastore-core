8a3ce29d3b4029a57a2b1ce0437c93aa90fb4c16
achingbrain <alex@achingbrain.net>
2020-07-29 13:36:55 +0100

    chore: remove gh dep version

--- test/keytransform.spec.js
+++ test/keytransform.spec.js
@@ -36,7 +36,7 @@ describe('KeyTransformDatastore', () => {
       'foo/bar/bazb',
       'foo/bar/baz/barb'
     ].map((s) => new Key(s))
-    await Promise.all(keys.map((key) => kStore.put(key, key.toBuffer())))
+    await Promise.all(keys.map((key) => kStore.put(key, key.uint8Array())))
     const kResults = Promise.all(keys.map((key) => kStore.get(key)))
     const mResults = Promise.all(keys.map((key) => mStore.get(new Key('abc').child(key))))
     const results = await Promise.all([kResults, mResults])
