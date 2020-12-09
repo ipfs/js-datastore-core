f0e324ffac4e2cb79d06d6bea91e3ac00cffaa4e
Hugo Dias <hugomrdias@gmail.com>
2020-12-09 15:58:56 +0000

    Update src/keytransform.js
    Co-authored-by: Vasco Santos <vasco.santos@moxy.studio>

--- src/keytransform.js
+++ src/keytransform.js
@@ -4,7 +4,7 @@ const { Adapter, utils } = require('interface-datastore')
 const map = utils.map
 /**
  * @typedef {import('interface-datastore/src/types').Datastore} Datastore
- * @typedef {import("interface-datastore/src/types").Options}Options
+ * @typedef {import("interface-datastore/src/types").Options} Options
  * @typedef {import("interface-datastore/src/types").Batch} Batch
  * @typedef {import('interface-datastore/src/key')} Key
  * @typedef {import('interface-datastore/src/adapter').Query} Query
