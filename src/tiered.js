028d79dd5c0fd7b89be1bd560b030001aa3a874b
achingbrain <alex@achingbrain.net>
2021-04-15 11:25:03 +0100

    chore: implement queryKeys for tiered datastore

--- src/tiered.js
+++ src/tiered.js
@@ -121,6 +121,14 @@ class TieredDatastore extends Adapter {
   query (q, options) {
     return this.stores[this.stores.length - 1].query(q, options)
   }
+
+  /**
+   * @param {KeyQuery} q
+   * @param {Options} [options]
+   */
+  queryKeys (q, options) {
+    return this.stores[this.stores.length - 1].queryKeys(q, options)
+  }
 }
 
 module.exports = TieredDatastore
